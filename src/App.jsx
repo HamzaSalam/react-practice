fc6d29b81b1c53400123b15dc7b567e852d0a792
HamzaSalam <hamza.salam786@gmail.com>
2024-03-26 01:06:17 +0500

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
+import Hero from "./components/Hero"
+import HomeCard from "./components/HomeCard"
+import JobListings from "./components/JobListings"
+import Navbar from "./components/Navbar"
 
 function App() {
-  const namee = "hamza";
-  const x= 7;
-  const y = 9;
-  const furit = ['mango','apple','orange']
+  // const namee = "hamza";
+  // const x= 7;
+  // const y = 9;
+  // const furit = ['mango','apple','orange']
 
-  const loggedIn = false;
+  // const loggedIn = false;
 
   return (
-    <div>
-      <h1>Hello {namee}</h1>
-      <p>Sum of {x} and {y} is {x+y}</p>
-      <ul>{furit.map((furites,index)=>(
-        <li key={index}>{furites}</li>
-      ))}
-      </ul>
-      {loggedIn ? <h1>Hello member</h1>:<h1>hello guest</h1>}
-    </div>
+    // <div>
+    //   <h1>Hello {namee}</h1>
+    //   <p>Sum of {x} and {y} is {x+y}</p>
+    //   <ul>{furit.map((furites,index)=>(
+    //     <li key={index}>{furites}</li>
+    //   ))}
+    //   </ul>
+    //   {loggedIn ? <h1>Hello member</h1>:<h1>hello guest</h1>}
+    // </div>
+    <>
+      <Navbar/>
+      <Hero title='Title here hello' subtitle='subtitle here' /> 
+      <HomeCard/>
+      <JobListings/>   
+
+    <section className="m-auto max-w-lg my-10 px-6">
+      <a
+        href="jobs.html"
+        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
+
+    </>
   )
 }
 
